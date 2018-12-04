4362d9cfaef3328ec58c8991d5c05cd0fd7da9f1
mob <innermond@yahoo.com>
2018-12-04 15:12:06 +0200

    test cases as structs

--- filemark_test.go
+++ filemark_test.go
@@ -32,13 +32,16 @@ func TestZeroFile(t *testing.T) {
 	})
 }
 
-// key = num of parts
-// value = marks actual to be returned
-var marksEmptySeparator = map[int][]int64{
-	0: []int64{0, 10},
-	1: []int64{0, 10},
-	2: []int64{0, 5, 10},
-	3: []int64{0, 4, 8, 10},
+// N = num of parts
+// M = marks actual to be returned
+var marksEmptySeparator = []struct {
+	N int
+	M []int64
+}{
+	{0, []int64{0, 10}},
+	{1, []int64{0, 10}},
+	{2, []int64{0, 5, 10}},
+	{3, []int64{0, 4, 8, 10}},
 }
 
 // TestEmptySeparator using a known file with empty separator
@@ -52,20 +55,23 @@ func TestEmptySeparator(t *testing.T) {
 	mk := New(f, "", fi.Size())
 
 	t.Run("Marks", func(t *testing.T) {
-		for n, actual := range marksEmptySeparator {
-			got := mk.Marks(n)
-			assert.Equal(t, got, actual, fmt.Sprintf("got %v instead %v", got, actual))
+		for _, tc := range marksEmptySeparator {
+			got := mk.Marks(tc.N)
+			assert.Equal(t, got, tc.M, fmt.Sprintf("got %v instead %v", got, tc.M))
 		}
 	})
 }
 
-// key = num of parts
-// value = marks actual to be returned
-var marksNewlineSeparator = map[int][]int64{
-	0: []int64{0, 20},
-	1: []int64{0, 20},
-	2: []int64{0, 10, 20},
-	3: []int64{0, 8, 16, 20},
+// N = num of parts
+// M = marks actual to be returned
+var marksNewlineSeparator = []struct {
+	N int
+	M []int64
+}{
+	{0, []int64{0, 20}},
+	{1, []int64{0, 20}},
+	{2, []int64{0, 10, 20}},
+	{3, []int64{0, 8, 16, 20}},
 }
 
 // TestEmptySeparator using a known file with empty separator
@@ -79,28 +85,23 @@ func TestNewlineSeparator(t *testing.T) {
 	mk := New(f, "\n", fi.Size())
 
 	t.Run("Marks", func(t *testing.T) {
-		for n, actual := range marksNewlineSeparator {
-			got := mk.Marks(n)
-			assert.Equal(t, got, actual, fmt.Sprintf("%d = got %v instead %v", n, got, actual))
+		for _, tc := range marksNewlineSeparator {
+			got := mk.Marks(tc.N)
+			assert.Equal(t, got, tc.M, fmt.Sprintf("%d = got %v instead %v", tc.N, got, tc.M))
 		}
 	})
 }
 
-<<<<<<< HEAD
-var emptySeparatorCases = map[int][]int64{
-	//0: []int64{20},
-	//1: []int64{20},
-	2: []int64{12, 20},
-	//3: []int64{8, 16, 20},
-=======
-// key = num of parts
-// value = marks actual to be returned
-var marksFancySeparator = map[int][]int64{
-	0: []int64{0, 19},
-	1: []int64{0, 19},
-	2: []int64{0, 13, 19},
-	3: []int64{0, 9, 17, 19},
->>>>>>> 4334fc90f01ee25784834d45e023ba08acce2382
+// N = num of parts
+// M = marks actual to be returned
+var marksFancySeparator = []struct {
+	N int
+	M []int64
+}{
+	{0, []int64{0, 19}},
+	{1, []int64{0, 19}},
+	{2, []int64{0, 13, 19}},
+	{3, []int64{0, 9, 17, 19}},
 }
 
 // TestEmptySeparator using a known file with empty separator
@@ -113,11 +114,9 @@ func TestFancySeparator(t *testing.T) {
 
 	mk := New(f, "xx", fi.Size())
 	t.Run("Marks", func(t *testing.T) {
-		for n, actual := range marksFancySeparator {
-			ps := mk.PartSize(n)
-			t.Log(n, ps)
-			got := mk.Marks(n)
-			assert.Equal(t, got, actual, fmt.Sprintf("%d = got %v instead %v", n, got, actual))
+		for _, tc := range marksFancySeparator {
+			got := mk.Marks(tc.N)
+			assert.Equal(t, got, tc.M, fmt.Sprintf("%d = got %v instead %v", tc.N, got, tc.M))
 		}
 	})
 }
